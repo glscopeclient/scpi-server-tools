9d3266401dc8fea2bb53d973e331eaa0fbf0064d
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2022-03-16 23:08:03 -0700

    Added BridgeSCPIServer to handle well-known commands.

---
+++ BridgeSCPIServer.h
@@ -0,0 +1,90 @@
+/***********************************************************************************************************************
+*                                                                                                                      *
+* scpi-server-tools                                                                                                    *
+*                                                                                                                      *
+* Copyright (c) 2012-2022 Andrew D. Zonenberg and contributors                                                         *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+#ifndef BridgeSCPIServer_h
+#define BridgeSCPIServer_h
+
+#include "SCPIServer.h"
+
+/**
+	@brief SCPI server supporting common commands shared by all scopehal bridge servers
+ */
+class BridgeSCPIServer : public SCPIServer
+{
+public:
+	BridgeSCPIServer(ZSOCKET sock);
+	virtual ~BridgeSCPIServer();
+
+protected:
+
+	virtual bool OnQuery(
+		const std::string& line,
+		const std::string& subject,
+		const std::string& cmd);
+
+	//Accessor methods for queries (must be overridden in derived classes)
+protected:
+
+	/**
+		@brief Returns the vendor / make of the instrument for *IDN? response
+	 */
+	virtual std::string GetMake() =0;
+
+	/**
+		@brief Returns the model name of the instrument for *IDN? response
+	 */
+	virtual std::string GetModel() =0;
+
+	/**
+		@brief Returns the serial number of the instrument for *IDN? response
+	 */
+	virtual std::string GetSerial() =0;
+
+	/**
+		@brief Returns the firmware version of the instrument for *IDN? response
+	 */
+	virtual std::string GetFirmwareVersion() =0;
+
+	/**
+		@brief Returns the number of analog channels
+	 */
+	virtual size_t GetAnalogChannelCount() =0;
+
+	/**
+		@brief Returns the set of currently valid sample rates, in Hz
+	 */
+	virtual std::vector<size_t> GetSampleRates() =0;
+
+	/**
+		@brief Returns the set of currently valid memory depths
+	 */
+	virtual std::vector<size_t> GetSampleDepths() =0;
+};
+
+#endif
